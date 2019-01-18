61f03b8f50d96f48166398c38b2bbddc0952ee79
Anita Mohan <anitachandlermohan@gmail.com>
2019-01-18 15:15:52 +0200

    trying to add category search

--- sakilaAPIstuff/startbootstrap-landing-page-gh-pages/sakilaAPI.js
+++ sakilaAPIstuff/startbootstrap-landing-page-gh-pages/sakilaAPI.js
@@ -1,6 +1,5 @@
 
-
-
+film_array[];
 function getAll(){
     let xhttp = new XMLHttpRequest();
     let url = "http://localhost:8080/api/film_list";
@@ -10,7 +9,9 @@ function getAll(){
         if (xhttp.readyState == 4 && xhttp.status == 200){
 
             let film_list = JSON.parse(xhttp.responseText);
-           
+            for(let i in film_list){
+                film_array.push
+            }
             for(let i in film_list){
                 let row = document.createElement("TR");
                 // title column //
@@ -24,6 +25,12 @@ function getAll(){
                 let desc_cell_text = document.createTextNode(JSON.stringify(film_list[i].description));
                 desc_cell.appendChild(desc_cell_text);
                 row.appendChild(desc_cell);
+
+                 //category //
+                let cat_cell = document.createElement("TD");
+                let cat_cell_text = document.createTextNode(JSON.stringify(film_list[i].category));
+                cat_cell.appendChild(cat_cell_text);
+                row.appendChild(cat_cell);
 
                 //append this row to table// 
                 table_body.appendChild(row);
@@ -47,7 +54,7 @@ function getFilmByID(){
     // document.getElementById("film_list").removeChild(document.getElementById("film_list").lastChild);
     let filmID = document.getElementById("inputFID").value;
     let xhttp = new XMLHttpRequest();
-    url = "http://localhost:8080/api/film_list/" + filmID;
+    let url = "http://localhost:8080/api/film_list/" + filmID;
     let film_table = document.createElement("TABLE");
     let table_body = document.createElement("TBODY");
     
@@ -73,51 +80,21 @@ function getFilmByID(){
     
 }
  function getFilmByCat(){
-     let xhttp = new XMLHttpRequest();
-     let filmCAT = document.getElementById("inputCAT").value;
-    let url = "http://localhost:8080/api/category/" + filmCAT;
-    console.log(url);
-    let film_table = document.createElement("TABLE");
-    let table_body = document.createElement("TBODY");
-    xhttp.onload = function (){
-        if (xhttp.readyState == 4 && xhttp.status == 200){
-
-            let film_list = JSON.parse(xhttp.responseText);
-            console.log(xhttp.responseText);
-            for(let i in film_list){
-                let row = document.createElement("TR");
-                // title column //
-                let title_cell = document.createElement("TD");
-                let title_cell_text = document.createTextNode( JSON.stringify(film_list[i].title));
-                title_cell.appendChild(title_cell_text);
-                row.appendChild(title_cell);
-
-                // description column //
-                let desc_cell = document.createElement("TD");
-                let desc_cell_text = document.createTextNode(JSON.stringify(film_list[i].description));
-                desc_cell.appendChild(desc_cell_text);
-                row.appendChild(desc_cell);
-
-                //append this row to table// 
-                table_body.appendChild(row);
-
-            }
-
-        }
-       
-    }
+     
+    let xhttp = new XMLHttpRequest();
+    let url = "http://localhost:8080/api/film_list";
+     xhttp.onreadystatechange = function (){
     
-    xhttp.open("GET", url);
-   
-    xhttp.setRequestHeader("Content-Type", "application/json")
-    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xhttp.responseType="json";
+     if (xhttp.readyState == 4 && xhttp.status == 200){
+        let film_list = JSON.parse(xhttp.responseText);
+        console.log(xhttp.responseText);
+        console.log(film_list);
+     }
+    
+    xhttp.open("GET", url,true);
     xhttp.send();
-    film_table.appendChild(table_body);
-    document.getElementById("filmlist").appendChild(film_table);
-
- }
-
+  }
+ }   
  function clearSearch(){
      let film_list_node = document.getElementById("filmlist");
      let first_node = film_list_node.firstChild;
