832c0a131607a7fb787b3f27a45804cf76f0a1c1
Anita Mohan <anitachandlermohan@gmail.com>
2019-01-18 16:06:10 +0200

    works on hosting site

--- sakilaAPIstuff/startbootstrap-landing-page-gh-pages/sakilaAPI.js
+++ sakilaAPIstuff/startbootstrap-landing-page-gh-pages/sakilaAPI.js
@@ -1,17 +1,19 @@
 
-film_array[];
+let film_array = [];
 function getAll(){
     let xhttp = new XMLHttpRequest();
-    let url = "http://localhost:8080/api/film_list";
+    let url = "http://192.168.1.116:8080/api/film_list";
     let film_table = document.createElement("TABLE");
     let table_body = document.createElement("TBODY");
     xhttp.onreadystatechange = function (){
         if (xhttp.readyState == 4 && xhttp.status == 200){
 
             let film_list = JSON.parse(xhttp.responseText);
-            for(let i in film_list){
-                film_array.push
-            }
+            for(let j in film_list){
+                film_array.push(film_list[j]);
+            } 
+            console.log(film_array);
+            
             for(let i in film_list){
                 let row = document.createElement("TR");
                 // title column //
@@ -38,8 +40,8 @@ function getAll(){
             }
 
         }
-       
-    }
+    }      
+ 
     
     xhttp.open("GET", url,true);
 
@@ -48,13 +50,14 @@ function getAll(){
     document.getElementById("filmlist").appendChild(film_table);
     
    
-}   
+} 
+  
 
 function getFilmByID(){
     // document.getElementById("film_list").removeChild(document.getElementById("film_list").lastChild);
     let filmID = document.getElementById("inputFID").value;
     let xhttp = new XMLHttpRequest();
-    let url = "http://localhost:8080/api/film_list/" + filmID;
+    let url = "http://192.168.1.116:8080/api/film_list/" + filmID;
     let film_table = document.createElement("TABLE");
     let table_body = document.createElement("TBODY");
     
@@ -79,22 +82,13 @@ function getFilmByID(){
     document.getElementById("filmlist").appendChild(film_table);
     
 }
- function getFilmByCat(){
-     
-    let xhttp = new XMLHttpRequest();
-    let url = "http://localhost:8080/api/film_list";
-     xhttp.onreadystatechange = function (){
-    
-     if (xhttp.readyState == 4 && xhttp.status == 200){
-        let film_list = JSON.parse(xhttp.responseText);
-        console.log(xhttp.responseText);
-        console.log(film_list);
-     }
-    
-    xhttp.open("GET", url,true);
-    xhttp.send();
-  }
- }   
+
+
+function findByCat(){
+    let filtered = film_array.filter(film=>film.category=="horror");
+
+ }
+
  function clearSearch(){
      let film_list_node = document.getElementById("filmlist");
      let first_node = film_list_node.firstChild;
